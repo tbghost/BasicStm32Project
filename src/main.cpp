41a737b29056941f8c4cfdcbfe9fcc60d9e90d03
Toberg <toberg@web.de>
2025-09-29 08:40:17 +0200

    Anpassung vsCode settings
    - colour scheme adapted
    - launch configuration adapted
    - new alternative settings file added
    - debug unittests functional

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,9 @@
 #include <stdint.h>
-//#include <iostream>
-//#include "hal_init.h"
-
-//void SystemClock_Config();
-//void Error_Handler();
 
 int main()
 {
-  //haL_init();
   
   uint32_t neverend{42};
-  //std::cout << "BasicProject" << std::endl;
   while(neverend == 42)
   {
 
@@ -18,24 +11,3 @@ int main()
 
   return 0;
 }
-
-
-
-//int main() {
-//    HAL_Init();
-//    SystemClock_Config();
-//
-//    while (1) {
-//        // Blink LED oder weitere Logik
-//    }
-//}
-//
-//void SystemClock_Config() {
-//    // Implementiere Taktkonfiguration hier
-//}
-//
-//void Error_Handler() {
-//    while (1) {
-//        // Fehlerbehandlung
-//    }
-//}
